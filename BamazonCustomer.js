a6fd1c7b269ae8a81a0b155490b04d5f424e0e6a
Andrea R <amrdev08@gmail.com>
2016-10-01 22:09:04 -0400

    initial commit

---
+++ BamazonCustomer.js
@@ -0,0 +1,24 @@
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+	host: "localhost",
+	port: 3306,
+	user: "root",
+	password: "",
+	database: "Bamazon"
+});
+
+connection.connect(function(err) {
+	if(err) {
+		throw err;
+	}
+	console.log("connected as id", connection.threadId);
+});
+
+connection.query('SELECT * FROM products', function(err,results) {
+	if(err) {
+		throw err;
+	}
+
+	console.log(results)
+});
